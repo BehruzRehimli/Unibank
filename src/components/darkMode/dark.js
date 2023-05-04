11caba3dd015ff50a01c8ad97593987028c04c24
BehruzRehimli <bahruzkr@code.edu.az>
2023-05-04 13:57:51 +0400

    fix bug

--- src/components/darkMode/dark.js
+++ src/components/darkMode/dark.js
@@ -21,26 +21,33 @@ export default function Dark(){
             body.classList.replace(darkTheme,lightTheme);
             e.target.classList.remove(clickedClass);
             localStorage.setItem ("theme","light");
-            theme=lightTheme
+            theme=lightTheme;
+            document.querySelector('.moon').style.display="none"
+            document.querySelector('.sun').style.display="inline-block"
+
         }
         else{
             body.classList.replace(lightTheme,darkTheme);
             e.target.classList.add(clickedClass);
             localStorage.setItem ("theme","dark");
             theme=darkTheme
-
+            document.querySelector('.moon').style.display="inline-block"
+            document.querySelector('.sun').style.display="none"
         }
     }
 
 
     return (
         <button className={theme==="dark"? clickedClass : ''} id="darkMode" onClick={(e)=> switchTheme(e)}>
-            <div className="dark-img" style={{width:"70%",height:'70%'}}>
-
-            </div>
-
+            <span style={{width:'70%',height:'70%',fontSize:'18px',display:'none'}} class="moon material-symbols-outlined">
+                light_mode
+                </span> 
+                <span style={{width:'70%',height:'70%',fontSize:'17px',color:'white'}} class="sun material-symbols-outlined">
+                sleep
+                </span>  
 
         </button>
 
     )
+
 }
